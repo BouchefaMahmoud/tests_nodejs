5b4a8952975f2fa6658a9c4d04cab170c93484c1
BouchefaMahmoud <mahmoudbouchefa06@gmail.com>
2020-05-14 14:33:10 +0200

    tests

--- test/integration-tests/server-tests.js
+++ test/integration-tests/server-tests.js
@@ -1,11 +1,22 @@
 var request  = require( 'supertest'), app = require('../../server/server')
+var persistence  =  require ('../../server/persistence')
+describe("Integration tests : ", ()=>{
 
-describe("homepage", ()=>{
-
-    describe("Les tests d'integration", ()=>{
-        it("premier test",(done)=>{
-            request(app).get("/").expect(200,done)
+  
+        it("insert data",(done)=>{
+            request(app).post("/insert-data","{keyData: simpleKey, data : simpleData.xls}").
+            expect(200,done)
         })
-    })
 
+        it("get Data using key",(done)=>{
+            key =  'simple-key'
+            value = 'value.xls'
+            persistence.insertData(key, value , response => {
+                request(app).get("/get-data","{keyData:"+key+"}").
+                expect('value.xls',done)
+            })
+    
+        
+      
+        })    
 })
