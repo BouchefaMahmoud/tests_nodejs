53d5d6dc93feb58b51b22556e645897a51f1b80a
BouchefaMahmoud <mahmoudbouchefa06@gmail.com>
2020-05-14 14:29:14 +0200

    add endpoints

--- server/server.js
+++ server/server.js
@@ -6,18 +6,35 @@ var persistence  = require('./persistence')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  res.send('home page')
-})
 
 app.post('/insert-data', (req, res)=>{
-
     persistence.insertData(req.query.keyData, req.query.data, (response)=>{
-      console.log(response)
-      res.send(response)
+      if(response === 'data saved successfully'){
+        res.status(200).end()
+      }else {
+        res.status(401).end()
+      }
     })
 })
 
+app.delete('/delete-data', (req, res) => {
+  persistence.deleteData(req.query.keyData, (response)=> {
+    res.end(response)
+  })
+})
+
+app.get('/get-data', (req, res)=>{
+  persistence.getData(req.query.keyData, (response)=>{
+    console.log(response)
+    res.send(response)
+  } )
+})
+
+app.get('/get-all-data', (req, res)=>{
+  persistence.getData( (response)=>{
+    res.send(response)
+  } )
+})
 
 
 app.listen(1234, function () {
