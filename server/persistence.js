13d1f1b48f1888e6764ea57093d1fa93d33614dc
Mahmoud.BOUCHEFA <mahmoud.bouchefa@bluesoft-group.com>
2020-05-14 14:58:09 +0200

    pers

--- server/persistence.js
+++ server/persistence.js
@@ -1,37 +1,37 @@
 var BDD = require('./BDD/data')
 
-persistence = {
+
     
-    isExistsData : function (keyData) {
-        return Object.keys(BDD).indexOf(keyData) > -1 
-    },
-
-    insertData : function (keyData, data, callback) {
-
-                    if( ! this.isExistsData(keyData) ) {
-                        BDD[keyData] = data 
-                        return callback('data saved successfully')
-                    }
-                    return callback('data already exists')
-                },
-
-    getData : function ( keyData, callback ){
-                    if(this.isExistsData(keyData))
-                        return BDD[keyData]
-                    return callback('data dosn\'t exists')    
-                },
-
-    getAllData :function (callback){
-                return callback(BDD.values)
-                },
-    deleteData: (keyData, callback)=>{
-        if(persistence.isExistsData(keyData)){
-            BDD.remove(keyData)
-            return callback('data has been deleted')
-        }
-        return callback('cannot delete data because it does\'t exists')
-    }  
+    persistence = {
     
+        isExistsData :  (keyData)=> {
+            return Object.keys(BDD).indexOf(keyData) > -1 
+        },
+        insertData :  (keyData, data, callback)=> {
+    
+                        if( ! persistence.isExistsData(keyData) ) {
+                            BDD[keyData] = data 
+                            return callback('data saved successfully')
+                        }
+                        return callback('data already exists')
+                    },
+    
+        getData : ( keyData, callback )=>{
+                        if(persistence.isExistsData(keyData))
+                            return callback(BDD[keyData])
+                        return callback('data dosn\'t exists')    
+                    },
+    
+        getAllDatas : (callback)=>{
+                    return callback(BDD.values)
+                    },
+        deleteData: (keyData, callback)=>{
+            if(persistence.isExistsData(keyData)){
+                BDD.remove(keyData)
+                return callback('data has been deleted')
+            }
+            return callback('cannot delete data because it does\'t exists')
+        }  
 }
 
 
